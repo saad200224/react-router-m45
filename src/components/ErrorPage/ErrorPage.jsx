3060da727d23d03f3df4baf648ec46468dc625bb
saad200224 <saad19021994@outlook.com>
2023-09-23 19:48:53 +0600

    first

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,18 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
     const error = useRouteError();
-    console.log(error)
+    // console.log(error)
     return (
         <div>
             <h2>Oops!!!!!!!!!</h2>
-            <p>{error.statusText}</p>
+            <p>{error.statusText || error.message}</p>
+            {
+                error.status === 404 && <div>
+                    <h3>Page not found</h3>
+                    <Link to='/'><button>Go Back</button></Link>
+                </div>
+            }
         </div>
     );
 };
